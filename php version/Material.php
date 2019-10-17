d635b48297559e72786ee396d223e394d689eea2
SOftHeroes <ShubhamJobanputra@gmail.com>
2019-10-16 21:11:47 -0700

    Uncommitted Code

---
+++ php version/Material.php
@@ -0,0 +1,217 @@
+<?php include "head2.php"?>
+<!--BANNER-->
+<header style="background-image: url('img/back1.jpg')">
+	<div id="particles-js"></div>
+	<div class="container">
+		<div class="row h-100">
+			<div class="col-md-10 align-self-center offset-md-1">
+				<div class="banner_text align-self-center">
+					<br/> <br/> <br/> <br/> <br/> <br/>
+				</div>
+				<!--/.banner_text-->
+			</div>
+		</div>
+	</div>
+	<!--/.container-->
+</header>
+<!--/BANNER-->
+<section class="portfolio_sec" id="portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<div class="title">
+					<h3>Our Products</h3>
+					<div class="underline"></div>
+				</div>
+				<!--/.title-->
+			</div>
+			<div class="w-100"></div>
+			<div class="w-100"></div>
+			<div class="col-md-12">
+				<div class="grid popup-gallery">
+					<div class="row">
+						<!-- <div class="col-md-4 grid-item branding" data-category="branding">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s1.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s1.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div> -->
+						<!-- <div class="col-md-4 grid-item ui_design" data-category="ui_design">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s2.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s2.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div> -->
+						<div class="col-md-4 grid-item ux_design" data-category="ux_design">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s3.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s3.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 grid-item ui_design" data-category="ui_design">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s4.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s4.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 grid-item Software" data-category="Software">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s5.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s5.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- <div class="col-md-4 grid-item branding" data-category="branding">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s6.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s6.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div> -->
+						<!-- <div class="col-md-4 grid-item ps" data-category="ps">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s7.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s7.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 grid-item prototype" data-category="prototype">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s8.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s8.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div> -->
+						<div class="col-md-4 grid-item prototype" data-category="prototype">
+							<div class="item-inner">
+								<div class="overlay">
+									<div class="view-list">
+										<a href="img/s9.jpg" data-toggle="lightbox" data-gallery="example-gallery" class="item popup-link"><img src="img/s9.jpg" width="375" height="355" alt="Thumb"/></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--/grid-->
+			</div>
+		</div>
+	</div>
+	<!--/.container-->
+</section>
+<!--/PORTFOLIO-->
+<!--CONTACT-->
+<section class="contacts" id="contactus">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<div class="title">
+					<h3>contact us</h3>
+					<div class="underline"></div>
+				</div>
+				<!--/.title-->
+			</div>
+			<div class="w-100"></div>
+			<div class="col-md-12 shadow">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="contact_form">
+							<form method="post" action="send_mail.php">
+								<div class="form-group">
+									<label for="name1">Name</label>
+									<input type="text" id="name" name="name" placeholder="Your name here"/>
+								</div>
+								<div class="form-group">
+									<label for="email1">Email</label>
+									<input type="email" id="email" name="email" placeholder="Your email here"/>
+								</div>
+								<div class="form-group">
+									<label for="message1">Message</label>
+									<textarea id="msg" name="msg" placeholder="Your message here" cols="10" rows="3"></textarea>
+								</div>
+								<button type="submit">send</button>
+							</form>
+						</div>
+						<!--/.contact_form-->
+					</div>
+					<div class="col-md-5 offset-md-1">
+						<div class="contact_right_single">
+							<img src="img/icon/contact1.png" alt=""/>
+							<p>:+91 7302627560 +91 9340777592</p>
+						</div>
+						<!--/.contact_right_single-->
+						<div class="contact_right_single">
+							<img src="img/icon/contact2.png" alt=""/>
+							<p>mhe.soluation@gmail.com</p>
+						</div>
+						<!--/.contact_right_single-->
+						<div class="contact_right_single">
+							<div class="Address">
+								<img src="img/icon/contact3.png" alt=""/>
+								<p>Flat No 506, Sagar Abhinav Heights Hosangabad Road Bhopal : 462026 (M.P)</p>
+							</div>
+						</div>
+						<!--/.contact_right_single-->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/.container-->
+</section>
+<!--/CONTACT-->
+<!--FOOTER-->
+<footer class="footers">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="copy_right">
+					<p>© 2019 All rights reserved </p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="footer_social">
+					<ul>
+						<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+						<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+						<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/.container-->
+</footer>
+<!--/FOOTER-->
+<a href="#" class="scrollup"><i class="fas fa-arrow-up fa-2x"></i></a>
+<script src="js/jquery-1.12.4.min.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script src="js/vendor/popper.min.js"></script>
+<script src="js/vendor/plugin.js"></script>
+<script src="js/main.js"></script>
+<!-- <script src="js/scrolling.js"></script> -->
+<!-- font Awesome -->
+<script src="https://kit.fontawesome.com/c645529b0c.js"></script>
+</body>
+</html>
